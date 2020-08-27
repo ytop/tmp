4485edbbea9b36419509083b0df417f38e58bdbf
Jonathan Yan <jonathan.yan1@huawei.com>
2020-08-27 00:50:47 -0400

    coco relative path

--- fruits_nuts/train.py
+++ fruits_nuts/train.py
@@ -5,7 +5,7 @@ from detectron2.config import get_cfg
 from detectron2.model_zoo import model_zoo
 from detectron2.data import DatasetCatalog
 from detectron2.data.datasets import register_coco_instances
-register_coco_instances("fruits_nuts", {}, "/store4/coco/data/trainval.json", "/store4/coco/data/images")
+register_coco_instances("fruits_nuts", {}, "../coco/data/trainval.json", "../coco/data/images")
 
 cfg = get_cfg()
 cfg.merge_from_file("/home/jyan/tmp/detectron2/configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
@@ -17,11 +17,11 @@ cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_50_FPN_3x/1378494
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.02
 cfg.SOLVER.MAX_ITER = (
-   30
+   60
 )  # 300 iterations seems good enough, but you can certainly train longer
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = (
     128
-)  # faster, and good enough for this toy dataset
+)  # faster, and good enough for this toy dataset , default 512
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # 3 classes (data, fig, hazelnut)
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
